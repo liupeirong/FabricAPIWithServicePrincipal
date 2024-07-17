b0b1c0d45353aa414c2ecda45d4a2605bbfd0671
Paige Liu <liupeirong@gmail.com>
2024-07-16 18:16:08 -0700

    added data plane api call with SP.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Identity.Client;
 using System.Net.Http.Headers;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
+# region config
 var authConfig = new
 {
   TenantId = "Your Entra ID Tenant ID",
@@ -11,13 +13,24 @@ var authConfig = new
   scopes = new string[] { "https://api.fabric.microsoft.com/.default" },
 };
 
-var fabricConfig = new
+var managementPlaneConfig = new
 {
   workspaceId = "Your Fabric Workspace ID",
   eventhouseId = "Your Fabric Eventhouse item ID for creating a new KQL database in",
   baseUrl = "https://api.fabric.microsoft.com/v1/",
+  databaseToBeCreated = "Name of the database you want to create",
 };
 
+var dataPlaneConfig = new
+{
+  databaseName = "Name of the existing Fabric KQL database",
+  baseUrl = "Your Fabric KQL DB query URL",
+  tableToBeCreated = "Name of the table you want to create",
+  tableToQuery = "Name of an existing table you want to query",
+};
+# endregion
+
+# region management_plane_operations
 IConfidentialClientApplication msalClient = ConfidentialClientApplicationBuilder
   .Create(authConfig.ClientId)
   .WithClientSecret(authConfig.ClientSecret)
@@ -29,23 +42,23 @@ AuthenticationResult msalAuthResult = await msalClient
 // Call Fabric API with the access token
 HttpClient client = new HttpClient();
 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", msalAuthResult.AccessToken);
-client.BaseAddress = new Uri(fabricConfig.baseUrl);
+client.BaseAddress = new Uri(managementPlaneConfig.baseUrl);
 
 // Get items
-String getUrl = $"workspaces/{fabricConfig.workspaceId}/items";
+String getUrl = $"workspaces/{managementPlaneConfig.workspaceId}/items";
 HttpResponseMessage response = await client.GetAsync(getUrl);
 string responseBody = await response.Content.ReadAsStringAsync();
 Console.WriteLine(responseBody);
 
 // Create a KQL database
-var postUrl = $"workspaces/{fabricConfig.workspaceId}/kqlDatabases";
+var postUrl = $"workspaces/{managementPlaneConfig.workspaceId}/kqlDatabases";
 var payload = new
 {
-  displayName = "kqldb1",
+  displayName = managementPlaneConfig.databaseToBeCreated,
   creationPayload = new
   {
     databaseType = "ReadWrite",
-    parentEventhouseItemId = fabricConfig.eventhouseId,
+    parentEventhouseItemId = managementPlaneConfig.eventhouseId,
   }
 };
 string payloadJson = System.Text.Json.JsonSerializer.Serialize(payload);
@@ -55,3 +68,56 @@ var postResultContent = await postResult.Content.ReadAsStringAsync();
 Console.WriteLine(postResult.IsSuccessStatusCode == true ?
   "Item created successfully!" :
   "Failed to create item: {0}, {1}", postResult.ReasonPhrase, postResultContent);
+#endregion
+
+# region data_plane_operations
+// create tables inside an existing KQL database
+// ensure the service principal is at least a user of the database, for example,
+// .add database <db_name> users ('aadapp=<sp_app_id>;<tenant_id>')
+IConfidentialClientApplication msalClientKusto = ConfidentialClientApplicationBuilder
+  .Create(authConfig.ClientId)
+  .WithClientSecret(authConfig.ClientSecret)
+  .WithAuthority(authConfig.Authority+authConfig.TenantId)
+  .Build();
+AuthenticationResult msalAuthResultKusto = await msalClientKusto
+  .AcquireTokenForClient(new[] {$"{dataPlaneConfig.baseUrl}/.default"}).ExecuteAsync();
+
+HttpClient clientKusto = new HttpClient();
+clientKusto.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", msalAuthResultKusto.AccessToken);
+clientKusto.BaseAddress = new Uri(dataPlaneConfig.baseUrl);
+var payloadDDL = new
+{
+  db = dataPlaneConfig.databaseName,
+  csl = $".create table {dataPlaneConfig.tableToBeCreated} ( Id:int64, Name:string, Value:float, Timestamp: datetime)",
+};
+string payloadJsonDDL = System.Text.Json.JsonSerializer.Serialize(payloadDDL);
+StringContent bodyDDL = new StringContent(payloadJsonDDL, Encoding.UTF8, "application/json");
+var postDDLResult = await clientKusto.PostAsync("/v1/rest/mgmt", bodyDDL);
+var postDDLResultContent = await postDDLResult.Content.ReadAsStringAsync();
+Console.WriteLine(postDDLResult.IsSuccessStatusCode == true ?
+  "table created successfully!" :
+  "Failed to create table: {0}, {1}", postDDLResult.ReasonPhrase, postDDLResultContent);
+
+// run a kql query
+var payloadQuery = new
+{
+  db = dataPlaneConfig.databaseName,
+  csl = $"{dataPlaneConfig.tableToQuery} | take 1",
+};
+string payloadJsonQuery = System.Text.Json.JsonSerializer.Serialize(payloadQuery);
+StringContent bodyQuery = new StringContent(payloadJsonQuery, Encoding.UTF8, "application/json");
+var postQueryResult = await clientKusto.PostAsync("/v1/rest/query", bodyQuery);
+var postQueryResultContent = await postQueryResult.Content.ReadAsStringAsync();
+if (postQueryResult.IsSuccessStatusCode == true) {
+  var jobj = JObject.Parse(postQueryResultContent);
+  var table = jobj["Tables"][0];
+  Console.WriteLine($"Table: {table["TableName"]}");
+  table["Rows"].ToList().ForEach(row =>
+  {
+    Console.WriteLine($"Row: {row}");
+  });
+} else {
+  Console.WriteLine("Failed to run query: {0}, {1}",
+    postQueryResult.ReasonPhrase, postQueryResultContent);
+}
+#endregion
